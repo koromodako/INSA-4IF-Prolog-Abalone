778f472ab58adfa3dc502d28a18a7c23a9be9eb8
Paul DAUTRY <paul.dautry@gmail.com>
2015-10-06 11:38:47 +0200

    Correction Game Over

--- gameOver.pl
+++ gameOver.pl
@@ -1,5 +1,47 @@
-% Base de faits
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                       Fonctions générales 
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%           Compte le nombre d'éléments dans une liste
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+count(_, [], 0) :- !. /* empty list, base case */
+count(X, [X|T], N) :- /* if X is in the head of the list */
+    count(X, T, N2), /* count on the tail (let this N2) */
+    N is N2 + 1.     /* and N is N2 + 1  */
+count(X, [Y|T], N) :- 
+    X \= Y,          /* if X is not in the head */
+    count(X, T, N).  /* just count the rest */
 
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                Game over et son test unitaire
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+gameOver(Joueur, Board) :- 
+	flatten(Board, InlineBoard),
+	count(Joueur, InlineBoard, N), 
+	N =< 8.	
+% -------------------------------------------------------------- %
+initGameOverUnitTest(
+[
+[0,0,0,0,0,-1,-1,-1,-1],
+[0,0,0,0,0,0,-1,-1,-1],
+[0,0,1,0,0,0,0,-1,-1],
+[1,1,1,0,0,0,0,2,-1],
+[1,1,1,0,0,0,2,2,2],
+[-1,1,0,0,0,0,2,2,2],
+[-1,-1,0,0,0,0,2,2,2],
+[-1,-1,-1,0,0,0,0,2,2],
+[-1,-1,-1,-1,0,0,0,2,2]
+]
+).
+% -------------------------------------------------------------- %
+gameOverUnitTest(Joueur) :- 
+	initGameOverUnitTest(Board),
+	gameOver(Joueur, Board).
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%
+% Base de faits
+%
 init(
 [
 [1,1,0,0,0,-1,-1,-1,-1],
@@ -13,39 +55,13 @@ init(
 [-1,-1,-1,-1,0,0,0,2,2]
 ]).
 
-% 
-%jouerCoup(Joueur, OldBoard, NewBoard) :-
-%
 
-%
-% Compte le nombre d'�l�ments dans une liste
-%
-count(_, [], 0) :- !. /* empty list, base case */
-count(X, [X|T], N) :- /* if X is in the head of the list */
-    count(X, T, N2), /* count on the tail (let this N2) */
-    N is N2 + 1.     /* and N is N2 + 1  */
-count(X, [Y|T], N) :- 
-    X \= Y,          /* if X is not in the head */
-    count(X, T, N).  /* just count the rest */
-
-%
-%	Condition de fin (GAME OVER !!!) 
-%
-gameOver() :- flatten(Board, InlineBoard),
-	count(1, InlineBoard, N) = 8.
-gameOver() :- flatten(Board, InlineBoard),
-	count(2, InlineBoard, N) = 8.
-	
 %
 % Boucle principale
 %
-play() :- gameOver, !.
 play(Winner) :-
 	init(Board),
 	flatten(Board, InlineBoard),
 	count(1, InlineBoard, N1),
 	count(2, InlineBoard, N2),
 	Winner is N1 + N2.
-
-
-
