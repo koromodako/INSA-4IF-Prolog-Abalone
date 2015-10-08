4e2cfcb869d8fab401c01b382bd316807bfb545b
Antoine Chabert <chabert.antoine@gmail.com>
2015-10-08 22:12:43 +0200

    Mise en place du module d'affichage (display) et du module pour le tester

--- display.pl
+++ display.pl
@@ -1,24 +1,18 @@
-init([
-[ 1, 1, 0, 0, 0,-1,-1,-1,-1],
-[ 1, 1, 0, 0, 0, 0,-1,-1,-1],
-[ 1, 1, 1, 0, 0, 0, 0,-1,-1],
-[ 1, 1, 1, 0, 0, 0, 0, 2,-1],
-[ 1, 1, 1, 0, 0, 0, 2, 2, 2],
-[-1, 1, 0, 0, 0, 0, 2, 2, 2],
-[-1,-1, 0, 0, 0, 0, 2, 2, 2],
-[-1,-1,-1, 0, 0, 0, 0, 2, 2],
-[-1,-1,-1,-1, 0, 0, 0, 2, 2]
-]).
+%% -----------------------------------------------------------------------------
+%% Module contenant les prédicats permettant d'afficher la grille de jeu
+:- module(display, [displayBoard/1]).
+%% -----------------------------------------------------------------------------
 
-play :-
-    init(Board),
-    displayBoard(Board).
-
+%% Affiche la grille du jeu avec les indices des lignes et des colonnes
+%% @param Board La grille de jeu à afficher
 displayBoard(Board):-
     nl, write('       A B C D EFGHI'),
     nl, write('       _________'),
     nl, displayRows(Board, 1),!, nl.
 
+%% Affiche les lignes de la grille de jeu
+%% @param Board La grille de jeu à afficher
+%% @param RowNb Numéro de ligne à afficher
 displayRows([], _).
 displayRows([Row|Rest], RowNb):-
     write(RowNb), write('  '),
@@ -29,18 +23,25 @@ displayRows([Row|Rest], RowNb):-
     NextRowNb is RowNb+1,
     displayRows(Rest, NextRowNb).
 
+%% Affiche une ligne de la grille de jeu
+%% @param Row La ligne à afficher
+%% @param ColNb Numéro de colonne à afficher
 displayRow(_, 10).
 displayRow([Element|Rest], ColNb):-
     displayPosition(Element),
     NextColNb is ColNb+1,
     displayRow(Rest, NextColNb).
 
+%% Affiche des espaces
+%% @param NbSpace Nombre d'espace à afficher
 displaySpaces(0).
 displaySpaces(NbSpace):-
     write(' '),
     NbSpaceLeft is NbSpace-1,
     displaySpaces(NbSpaceLeft).
 
+%% Affiche une case de la grille
+%% @param Element L'élément à afficher
 displayPosition(-1).
 displayPosition(Element):-
     write(Element), write(' ').
