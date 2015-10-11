cd1ff41ada3ca4fdfb8080fc12bead56e9787506
Pierre Jarsaillon <pierre.jarsaillon63@gmail.com>
2015-10-11 16:38:17 +0200

    Ajout de l'IA aléatoire et du module de lancement du jeu

---
+++ iaRandom.pl
@@ -0,0 +1,56 @@
+%% -----------------------------------------------------------------------------
+%% Module contenant l'IA réalisant des déplacements aléatoires
+:- module(iaRandom, [playTurn/2]).
+%% -----------------------------------------------------------------------------
+
+:- use_module('board.pl').
+:- use_module('movable.pl').
+:- use_module('move.pl').
+:- use_module('display.pl').
+
+%% Trouve le prochaine déplacement à effectuer, de façon aléatoire
+%% @param Board Plateau de jeu
+%% @param Player Le joueur qui doit jouer son tour
+%% @param Line La ligne (basée 1) de la case de départ
+%% @param Col La colonne (basée 1) de la case de départ
+%% @param LineDest La ligne (basée 1) de la case d'arrivée
+%% @param ColDest La colonne (basée 1) de la case d'arrivée
+nextMove(Board, Player, Line, Col, NextLine, NextCol) :-
+	
+	% On récupère la taille du plateau
+	length(Board, Size),
+	MaxSize is Size + 1,
+	
+	% On réessaye de trouver un déplacement tant qu'on en possède pas un 
+	% de faisable.
+	repeat,
+
+	% On tire au sort une case
+	random(1, MaxSize, Line), random(1, MaxSize, Col),
+	
+	% On vérifie qu'elle appartient bien au joueur
+	board:squareContains(Board, Line, Col, Player),
+	
+	% On tire au sort la direction
+	random(-1, 2, DiffLine), random(-1, 2, DiffCol),
+	NextLine is Line + DiffLine, NextCol is Col + DiffCol,
+	
+	% On vérifie que le déplacement est possible
+	movable:isMovable(Board, Line, Col, NextLine, NextCol),
+	
+	!.
+
+%% Joue le tour du joueur passé en argument.
+%% @param Board Plateau de jeu
+%% @param Player Le joueur qui doit jouer son tour
+playTurn(Board, Player) :-
+	%display:displayBoard(Board),
+	nextMove(Board, Player, Line, Col, NextLine, NextCol),
+	%print('Line : '), print(Line),nl,
+	%print('Col : '),print(Col),nl,
+	%print('NextLine : '),print(NextLine),nl,
+	%print('NextCol : '),print(NextCol),nl,
+	moveMarbles(Board, Line, Col, NextLine, NextCol, Board).
+	%display:displayBoard(Board).
+	
+	
---
+++ play.pl
@@ -0,0 +1,41 @@
+%% -----------------------------------------------------------------------------
+%% Module permettant de jouer à abalone en utilisant les IA
+:- module(play, []).
+%% -----------------------------------------------------------------------------
+
+:- use_module('board.pl').
+:- use_module('gameOver.pl').
+:- use_module('iaRandom.pl').
+
+%% Joue le tour du joueur passé en argument
+%% @param Board Plateau de jeu
+%% @param Player Le joueur qui doit jouer son tour
+%% @param I Variable temporaire, pour limiter le nombre de boucles (dev)
+play(Board, Player, I) :-
+	I =< 10,
+	
+	% On s'arrête si game over (todo : afficher msg...)
+	not(gameOver:gameOver(Player, Board)),
+	
+	print('Tour du joueur '), print(Player), nl,
+	iaRandom:playTurn(Board, Player),
+	
+	% Tour suivant en changeant de joueur
+	sleep(1),
+	(
+		(
+			Player == 1, play(Board, 2, I+1)
+		)
+		;
+		(
+			Player == 2, play(Board, 1, I+1)
+		)
+	),
+	
+	display:displayBoard(Board)
+	.
+
+%% Lance le jeu après avoir initialisé le plateau
+play() :-
+	board:initBoard(Board),
+	play(Board, 1).
