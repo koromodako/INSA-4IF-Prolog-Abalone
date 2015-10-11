66da7f9a46151d1b3b0c87c8295d6c5ffd4b882f
Antoine Chabert <chabert.antoine@gmail.com>
2015-10-11 17:49:52 +0200

    Amélioration de l'affichage

--- display.pl
+++ display.pl
@@ -6,20 +6,23 @@
 %% Affiche la grille du jeu avec les indices des lignes et des colonnes
 %% @param Board La grille de jeu à afficher
 displayBoard(Board):-
-    nl, write('       A B C D EFGHI'),
-    nl, write('       _________'),
-    nl, displayRows(Board, 1),!, nl.
+    nl, write('           A B C D E'),
+    nl, write('          / / / / /  F'),
+    nl, write('        ─────────   / G'),
+    nl, displayRows(Board, 1),!,
+    write('        ─────────'), nl.
 
 %% Affiche les lignes de la grille de jeu
 %% @param Board La grille de jeu à afficher
 %% @param RowNb Numéro de ligne à afficher
 displayRows([], _).
 displayRows([Row|Rest], RowNb):-
-    write(RowNb), write('  '),
     NbSpace is abs(5-RowNb),
     displaySpaces(NbSpace),
+    write(RowNb), write('─'),
+    displaySeparatorBegin(RowNb), write(' '),
     displayRow(Row, 1),
-    write(' '), nl,
+    displaySeparatorEnd(RowNb), nl,
     NextRowNb is RowNb+1,
     displayRows(Rest, NextRowNb).
 
@@ -40,8 +43,28 @@ displaySpaces(NbSpace):-
     NbSpaceLeft is NbSpace-1,
     displaySpaces(NbSpaceLeft).
 
+%% Affiche le caractère pour délimiter la grille (début de ligne)
+%% @param RowNb Numéro de ligne
+displaySeparatorBegin(RowNb):-
+    RowNb < 5, write('/');
+    RowNb == 5, write('|');
+    RowNb > 5, write('\\').
+
+%% Affiche le caractère pour délimiter la grille (fin de ligne)
+%% @param RowNb Numéro de ligne
+displaySeparatorEnd(RowNb):-
+    RowNb == 1, write('\\  / H');
+    RowNb == 2, write('\\  / I');
+    RowNb == 3, write('\\  /');
+    RowNb < 5, write('\\');
+    RowNb == 5, write('|');
+    RowNb > 5, write('/').
+
 %% Affiche une case de la grille
 %% @param Element L'élément à afficher
 displayPosition(-1).
 displayPosition(Element):-
-    write(Element), write(' ').
+    Element == -1;
+    Element == 0, write('.'), write(' ');
+    Element == 1, write('x'), write(' ');
+    Element == 2, write('o'), write(' ').
