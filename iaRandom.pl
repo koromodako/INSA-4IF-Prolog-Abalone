6edb862b72259d27d9b48c7d230ff120d86847c1
Pierre Jarsaillon <pierre.jarsaillon63@gmail.com>
2015-10-13 11:38:25 +0200

    Mise à jour IA Random + Movable : coups possiblespour un joueur

--- iaRandom.pl
+++ iaRandom.pl
@@ -1,6 +1,6 @@
 %% -----------------------------------------------------------------------------
 %% Module contenant l'IA réalisant des déplacements aléatoires
-:- module(iaRandom, [playTurn/2]).
+:- module(iaRandom, [playTurn/3]).
 %% -----------------------------------------------------------------------------
 
 :- use_module('board.pl').
@@ -33,6 +33,8 @@ nextMove(Board, Player, Line, Col, NextLine, NextCol) :-
 	
 	% On tire au sort la direction
 	random(-1, 2, DiffLine), random(-1, 2, DiffCol),
+	not((DiffLine == 0, DiffCol == 0)),
+	
 	NextLine is Line + DiffLine, NextCol is Col + DiffCol,
 	
 	% On vérifie que le déplacement est possible
@@ -43,14 +45,14 @@ nextMove(Board, Player, Line, Col, NextLine, NextCol) :-
 %% Joue le tour du joueur passé en argument.
 %% @param Board Plateau de jeu
 %% @param Player Le joueur qui doit jouer son tour
-playTurn(Board, Player) :-
+playTurn(Board, Player, NewBoard) :-
 	%display:displayBoard(Board),
 	nextMove(Board, Player, Line, Col, NextLine, NextCol),
-	%print('Line : '), print(Line),nl,
-	%print('Col : '),print(Col),nl,
-	%print('NextLine : '),print(NextLine),nl,
-	%print('NextCol : '),print(NextCol),nl,
-	moveMarbles(Board, Line, Col, NextLine, NextCol, Board).
+	print('Line : '), print(Line),nl,
+	print('Col : '),print(Col),nl,
+	print('NextLine : '),print(NextLine),nl,
+	print('NextCol : '),print(NextCol),nl,
+	moveMarbles(Board, Col, Line, NextCol, NextLine, NewBoard).
 	%display:displayBoard(Board).
 	
 	
