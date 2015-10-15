1535d05d1d248b6cd8045e34ce580bbacdf83fae
hverlin <hugues.verlin@gmail.com>
2015-10-15 17:55:41 +0200

    implémentation de l'heuristique presque finie, faut juste savoir si on prend la valeur absolue pour la fin ou pas

--- geometricScoreFunction.pl
+++ geometricScoreFunction.pl
@@ -26,9 +26,6 @@
 
 :- use_module('board.pl').
 
-
-	
-
 %% Calcule un couple (Line, Col) contenant une bille de la couleur du Player
 %% @param Player numéro du joueur (1 ou 2)
 %% @param Board le plateau a analyser
@@ -40,6 +37,7 @@ teamMarbleCoord(Player, Board, Line, Col) :-
 
 %% Calcule le centre de masse d'une couleur (=la somme des coordonnees de l'ensemble des billes d'une couleur présentes
 %% sur le plateau / nb billes d'une couleur)
+%%
 %% @param Player numéro du joueur (1 ou 2)
 %% @param Board le plateau a analyser
 %% @param SumCol la somme des coordonnees en colonne
@@ -55,6 +53,7 @@ getBarycenter(Player, Board, BarycenterLine, BarycenterCol) :-
 
 %% Calcul le point objectif du plateau en faisant la moyenne des barycentres des boules
 %% des deux joueurs et du centre du plateau.
+%%
 %% @param BaryLineBlack barycentre du noir selon les lignes
 %% @param BaryColBlack barycentre du noir selon les colonnes
 %% @param BaryLineWhite barycentre du blanc selon les lignes
@@ -65,9 +64,104 @@ getAimPoint(BaryLineBlack, BaryColBlack, BaryLineWhite, BaryColWhite, AimPointLi
 	AimPointLine is (BaryLineBlack + BaryLineWhite + 4.5)/3,
 	AimPointCol is (BaryColBlack + BaryColWhite + 4.5)/3.
 	
+%% Calcul de la distance de Manhattan entre une bille et le point visé	
+computeDistance(Line0, Col0, Line1, Col1, OldResult, NewResult):- 
+	(
+		(
+			Line0 == Line1,
+			Temp is abs(Col0 - Col1),
+			NewResult is (OldResult + Temp) 
+		);
+		(
+			Col0 == Col1,
+			Temp is abs(Line0 - Line1),
+			NewResult is (OldResult + Temp) 
+		);
+		(
+			D0temp is (Col0 - Line0),
+			D1temp is (Col1 - Line1),
+			D0temp == D1temp,
+			Temp is abs(Line1 - Line0),
+			NewResult is (OldResult + Temp)
+		);
+		(
+			Sline is sign(Line1 - Line0),
+			Scol is sign(Col1 - Col0),
+			(
+				Sline == 1,
+				Scol == 1,
+				Result is OldResult + 1,
+				L0 is (Line0 + 1),
+				C0 is (Col0 + 1),
+				computeDistance(L0, C0, Line1, Col1, Result, NewResult)
+			);
+			(
+				Sline == (-1),
+				Scol == (-1),
+				Result is OldResult + 1,
+				L0 is (Line0 - 1),
+				C0 is (Col0 - 1),
+				computeDistance(L0, C0, Line1, Col1, Result, NewResult)
+			);
+			(
+				Distline is abs(Line1 - Line0),
+				Distcol is abs(Col1 - Col0), 
+				TempSum is (Distcol + Distline),
+				NewResult is (OldResult + TempSum) 
+			)
+		)
+	).
+
+
+%% vrai si sign(A) == sign(B)
+compareSign(A, B):-
+	X is sign(A),
+	Y is sign(B),
+	X == Y.
+
+%% Retourne le score pour une bille donnée
+%% et un joueur donné
+
+computeScoreDistance(Player, Board, AimPointLine, AimPointCol, Score):- 
+	teamMarbleCoord(Player, Board, Line, Col),
+	computeDistance(AimPointLine, AimPointCol, Line, Col, 0, Score).
+	
+%% Retourne le nombre de billes en dehors du plateau pour un joueur
+%% donné, multiplié par 10 (arbitraire).
+%% ceci donne le score des billes en dehors du plateau
+
+scoreMarblesOut(Player, Board, Score):-
+	count(Player, Board, N),  
+	Score is (N * 10).
+
+%% Retourne le score total dans Result pour un Player donné
+
+computeTotalScoreDistance(Player, Board, AimPointLine, AimPointCol, Result):- 
+	findall(Score, computeScoreDistance(Player, Board, AimPointLine, AimPointCol, Score), Scores),
+	sum_list(Scores, ScoreIn),
+	scoreMarblesOut(Player, Board, ScoreOut),
+	Result is (ScoreIn + ScoreOut).
+	
+%% On calcule la différence entre les deux scores, et cela nous donne la valeur de 
+%% notre heuristique pour ce plateau et pour la couleur examinée.
+%% Le meilleur score est le plus petit.
+%% Je suis noir. Pour savoir si un coup est bon, il faut que je cherche à maximiser 
+%% Sb - Sn (= les blancs doivent avoir un score énorme, et moi un petit score)
+compareScore(Player, ScoreWhite, ScoreBlack, FinalScore):-
+	(
+		(
+			Player == 1,
+			FinalScore is (ScoreWhite - ScoreBlack)
+		);
+		(
+			Player == 2,
+			FinalScore is (ScoreBlack - ScoreWhite)		
+		)
+	).
+
 %% Trouve le score calculé selon l'heuristique
 %% @param Board le plateau de jeu à analyser
-geometricScore(Board, AimPointLine, AimPointCol) :-
+geometricScore(Board, Player, FinalScore) :-
 	board:initBoard(Board),
 	
 	%% 1/ Calcul du centre de masse des deux couleurs BaryLine/ColBlack et BaryLine/ColWhite
@@ -75,4 +169,14 @@ geometricScore(Board, AimPointLine, AimPointCol) :-
 	getBarycenter(2, Board, BaryLineWhite, BaryColWhite),
 	
 	%% 2/ Calcul de la moyenne R des barycentres et du centre du plateau Cp
-	getAimPoint(BaryLineBlack, BaryColBlack, BaryLineWhite, BaryColWhite, AimPointLine, AimPointCol).
+	getAimPoint(BaryLineBlack, BaryColBlack, BaryLineWhite, BaryColWhite, AimPointLine, AimPointCol),
+	
+	%% 3/ Calcul des distances de Manhattan
+	computeTotalScoreDistance(1, Board, AimPointLine, AimPointCol, ScoreDistanceBlack),
+	computeTotalScoreDistance(2, Board, AimPointLine, AimPointCol, ScoreDistanceWhite),
+	
+	%% 4/ Comparaison des deux scores
+	compareScore(Player, ScoreDistanceWhite, ScoreDistanceBlack, FinalScore).
+
+	
+	
