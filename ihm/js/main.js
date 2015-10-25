b88557faa7386d1ebe1dc0e7442d7a1921bded31
Antoine Chabert <chabert.antoine@gmail.com>
2015-10-25 22:09:04 +0100

    Ajout des infos dans le debug

--- ihm/js/main.js
+++ ihm/js/main.js
@@ -1,6 +1,41 @@
 $(function() {
-    $('g.tile').click( function(){
-        $(this).addClass('active');
+
+    var DEBUG = 1;
+
+    $( document ).ready(function(){
+
+        // hide debug section if not in debug mode
+        if(!DEBUG){
+            $( "#debug" ).hide();
+        }
+
     });
+
+
+    var currentSelectedTile = null;
+    $('g.tile').click( function(){
+        if (currentSelectedTile) {
+            currentSelectedTile.removeClass('active');
+        }
+        $(this).addClass('active');
+        currentSelectedTile = $(this);
+
+        if(DEBUG) {
+            // print debug tiles infos
+            var circleEmptyTitle = $(this).children("circle.emptyTile").first();
+            $( "#printCx" ).text(circleEmptyTitle.attr("cx"));
+            $( "#printCy" ).text(circleEmptyTitle.attr("cy"));
+            if($(this).has("circle.blackMarble").length) {
+                $( "#printColor" ).text("Black marble");
+            } else if($(this).has(".whiteMarble").length) {
+                $( "#printColor" ).text("White marble");
+            } else {
+                $( "#printColor" ).text("Empty tile");
+            }
+        }
+
+
+    });
+
 });
 
