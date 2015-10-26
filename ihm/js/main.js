6f30a7557f3023a378f8fd7513e0a323579e9a1e
Estelle <elepeigneux@gmail.com>
2015-10-26 13:49:53 +0100

    ajout deplacement d'une bille sur une case vide

--- ihm/js/main.js
+++ ihm/js/main.js
@@ -11,10 +11,33 @@ $(function() {
 
     });
 
+    function isNextTo(currentTile, selectedTile) {
+        var dCx = $(selectedTile).children("circle.emptyTile").attr("cx") - $(currentTile).children("circle.emptyTile").attr("cx");
+        var dCy = $(selectedTile).children("circle.emptyTile").attr("cy") - $(currentTile).children("circle.emptyTile").attr("cy");
+        var distance = Math.sqrt(Math.pow(dCx,2)+Math.pow(dCy,2));
+        return distance<(20*2);
+    };
+
+    function move(currentTile, nextTile) {
+        if($(nextTile).children("circle.marble").length == 0) {
+            // if no marble -> empty tile
+
+            // move the xml object to the right tile
+            var marble = $(currentTile).children("circle.marble");
+            $(nextTile).append(marble);
+            var cx = $(nextTile).children("circle.emptyTile").attr("cx");
+            var cy = $(nextTile).children("circle.emptyTile").attr("cy");
+            $(marble).attr("cx", cx);
+            $(marble).attr("cy", cy);
+        }
+    }
 
     var currentSelectedTile = null;
     $('g.tile').click( function(){
         if (currentSelectedTile) {
+            if($(currentSelectedTile).children("circle.marble") && isNextTo(currentSelectedTile, this)) {
+                move(currentSelectedTile, this);
+            }
             currentSelectedTile.removeClass('active');
         }
         $(this).addClass('active');
