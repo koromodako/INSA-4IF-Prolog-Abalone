25f21331b3ba8c8534c3495944458b045ae4fbdb
Antoine Chabert <chabert.antoine@gmail.com>
2015-10-29 20:29:42 +0100

    Ajout des compteurs pour connaitre le nombre de bille sorties avant de perdre

--- gameOver.pl
+++ gameOver.pl
@@ -1,13 +1,14 @@
 %% -----------------------------------------------------------------------------
 %% Module contenant les prédicats permettant d'afficher la grille de jeu
-:- module(gameOver, [gameOver/2]).
+:- module(gameOver, [gameOver/2, getNumberLeft/3]).
 %% -----------------------------------------------------------------------------
 
 :- use_module(board).
 
-%% 
-%% Game over et son test unitaire
-%% 
+%% Game over
+%% Vérification de la condition d'arrêt du jeu
+%% @param Board Plateau de jeu
+%% @param Player Le joueur à vérifier
 gameOver(Joueur, Board) :- 
     flatten(Board, InlineBoard),
     count(Joueur, InlineBoard, N),
